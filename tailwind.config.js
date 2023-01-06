b602e46dfa08d7b691dd435ff8d8accb15d5d9ed
Claudia Parravano <01claudia.parravano@gmail.com>
2023-01-06 17:45:26 +0100

    fix 2 tailwind exports

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.{png,svg,jpg, otf}",
   ],
   theme: {
     screens: {
