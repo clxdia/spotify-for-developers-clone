a6446675fe1f2fc504ef60e999980a561bca1c28
Claudia Parravano <01claudia.parravano@gmail.com>
2023-01-09 17:41:39 +0100

    fix community page, pre strapi

--- pages/community.js
+++ pages/community.js
@@ -7,8 +7,8 @@ const community = () => {
         <div className="h-[0.9rem] w-[300px] min-w-[300px] bg-greenify"></div>
         <div className="h-[0.9rem] bg-black_code w-[100%]"></div>
       </div>
-      <div className="bg-community max-h-[350px] bg-cover w-[100%] flex items-center">
-        <h1 className="text-[96px] sm:text-[80px] text-white xs:text-[56px] leading-tight font-circular_bold xs:ml-0 ml-[10vw] xs:max-w-[100%] max-w-[40vw] ">
+      <div className="bg-community bg-cover h-[400px] w-[100%] m-auto flex items-center">
+        <h1 className="xs:p-5 xs:w-[100%] text-[80px] w-[750px] m-auto text-white xs:text-[56px] leading-tight font-circular_bold xs:ml-0 ml-[16vw] sm:ml-[4vw] xs:max-w-[90%] xs:m-auto ">
           Developer News
         </h1>
       </div>
