9a838ff9093b15c01e9e08431a2ac711747d2049
Claudia Parravano <01claudia.parravano@gmail.com>
2023-01-12 18:44:34 +0100

    add loading and error catch in community news

--- pages/community.js
+++ pages/community.js
@@ -1,18 +1,34 @@
+import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import NewsUI from "../components/NewsUI";
+import loading from "/public/loading.gif";
 
 const Community = () => {
-  const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState(null);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    const fetchNews = async () => {
-      const res = await fetch(
+    setTimeout(() => {
+      fetch(
         `https://newsdata.io/api/1/news?apikey=${process.env.NEXT_PUBLIC_API_KEY}&language=it&from_date=2023-01-12&to_date=2023-01-12&category=technology`
-      );
-      const data = await res.json();
-      setArticles(data.results);
-    };
-    fetchNews();
+      )
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data from the api");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setArticles(data.results);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
   }, []);
 
   return (
@@ -27,13 +43,25 @@ const Community = () => {
         </h1>
       </div>
       <main>
-        {articles.length === 0 ? (
-          <div>Loading....</div>
-        ) : (
+        {isPending && (
+          <article className="m-auto text-center w-[65%] mb-10 h-[20rem] mt-32 gap-0 flex flex-col justify-center sm:w-[75%] xs:w-[95%]">
+            <p className="text-grey text-[13px]">Loading news</p>
+            <Image src={loading} className="w-[150px] h-auto m-auto"></Image>
+          </article>
+        )}
+        {error && (
+          <article className="m-auto text-center w-[65%] mb-10 h-[20rem] mt-10 flex flex-col justify-center gap-4 sm:w-[75%] xs:w-[95%]">
+            <h3 className="text-menu_grey text-[16px]">
+              Something went wrong, please try again in a bit.
+            </h3>
+            <p className="text-grey text-[13px]">{error}</p>
+          </article>
+        )}
+
+        {articles &&
           articles.map((article) => (
             <NewsUI article={article} key={article.pubDate} />
-          ))
-        )}
+          ))}
       </main>
     </div>
   );
