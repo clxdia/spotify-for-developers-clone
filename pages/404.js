fc91b9833ba6c06f4d8a3817ad8360e4440a0d55
Claudia Parravano <01claudia.parravano@gmail.com>
2023-01-20 16:59:31 +0100

    fix 404 page

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ export default function Custom404() {
         404 - Page not found
       </h1>
       <p className="font-circular_light text-[18px] mb-2">
-        Oops! It seems like we can't find the page you are looking for.
+        Oops! It seems like we can&#39;t find the page you are looking for.
       </p>
       <button className="bg-greenify w-fit text-[14px] xs, sm:mb-3 text-white p-4 pl-10 pr-10 rounded-[50px] uppercase font-sans font-bold tracking-widest mt-3 hover:bg-link hover:ease-in-out">
         <Link href="/">go home</Link>
